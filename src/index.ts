7f6c40e8e520aeca86d4e1e4dce80c61164c8af9
TTFTCUTS <emperorlexx@hotmail.com>
2022-03-10 03:56:12 +0000

    First test stuff

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run(): Promise<void> {
+    try {
+        
+        const name: string = core.getInput("who-to-greet");
+
+        console.log("Hello ${name}!");
+
+        const time: string = new Date().toTimeString();
+
+        core.setOutput("time", time);
+
+        const payload: string = JSON.stringify(github.context.payload, null, 2);
+
+        console.log("Event payload: ${payload}");
+
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
+    }
+}
+
+run();
