a76da185a122a13df902ba9882369bf13b296465
TTFTCUTS <emperorlexx@hotmail.com>
2022-03-14 04:54:19 +0000

    Set up for action test

--- src/localtest.ts
+++ src/localtest.ts
@@ -2,9 +2,9 @@ import * as process from "./processor";
 import * as pathLib from "path";
 
 async function run(): Promise<void> {
-    const relativePath: string = pathLib.join("test-data","gigas_process");//"process"
-    const sourceLanguage: string = "english";//"sourcelang";
-    const outputLanguages: Array<string> = ["braz_por","french","german","polish","russian","simp_chinese","spanish"]//["testlang1", "testlang2"];
+    const relativePath: string = pathLib.join("test-data","process");//"gigas_process");//
+    const sourceLanguage: string = "sourcelang";//"english";//
+    const outputLanguages: Array<string> = ["testlang1", "testlang2"];//["braz_por","french","german","polish","russian","simp_chinese","spanish"]//
 
     await process.processLoc(relativePath, sourceLanguage, outputLanguages);
 }
